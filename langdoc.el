bc516dbbc8d5588dfeb3baab55794db9e2268dfb
Tomoya Tanjo <ttanjo@gmail.com>
2013-06-06 08:31:38 +0900

    Minor document fix

--- langdoc.el
+++ langdoc.el
@@ -24,7 +24,7 @@
 
 ;; This library helps you to define help document mode for various languages.
 ;; `langdoc-define-help-mode' makes a major mode for help document
-;; and a function to show a description of a symbol. It takes at least six
+;; and a function to show a description of a symbol.  It takes at least six
 ;; arguments.
 ;;
 ;; First three arguments are to define the help document mode.
@@ -40,7 +40,7 @@
 ;; Next three arguments are to define MODE-PREFIX-describe-symbol.
 ;; * POINTED-SYM-FN
 ;;   Function name which returns the string pointed by
-;;   the cursor. This function takes no arguments.
+;;   the cursor.  This function takes no arguments.
 ;; * SYMBOL
 ;;   List of strings which is used to complete words.
 ;; * MAKE-DOCUMENT-FN
@@ -107,32 +107,36 @@
                                     &optional link-regexp linked-str-fn
                                       make-link-fn prefix-str suffix-str)
   "Define help-mode and describe-symbol functions.
-It defines MODE-PREFIX-mode which is a major mode to show help strings, and
-defines MODE-PREFIX-describe-symbol to show help strings in MODE-PREFIX-mode.
-MODE-PREFIX-describe-symbol takes a string to show a full documentation in a help buffer.
-DESCRIPTION is a description of MODE-PREFIX-mode. HELPBUF-NAME is a buffer name
-for MODE-PREFIX-mode.
+It defines MODE-PREFIX-mode which is a major mode to show help strings,
+and defines MODE-PREFIX-describe-symbol to show help strings in
+MODE-PREFIX-mode.  MODE-PREFIX-describe-symbol takes a string to show
+a full documentation in a help buffer.  DESCRIPTION is a description
+of MODE-PREFIX-mode.  HELPBUF-NAME is a buffer name for
+MODE-PREFIX-mode.
 
-POINTED-SYM-FN is a function which recieves no arguments and returns a string
-pointed by the cursor. MODE-PREFIX-describe-symbol uses POINTED-SYM-FN when
-it is interactively called. SYMBOLS is a list of strings to complete the
-argument of MODE-PREFIX-describe-symbol. MAKE-DOCUMENT-FN is a function
-which takes a string and returns the string which is a full document of the argument.
+POINTED-SYM-FN is a function which recieves no arguments and returns a
+string pointed by the cursor.  MODE-PREFIX-describe-symbol uses
+POINTED-SYM-FN when it is interactively called.  SYMBOLS is a list of
+strings to complete the argument of MODE-PREFIX-describe-symbol.
+MAKE-DOCUMENT-FN is a function which takes a string and returns the
+string which is a full document of the argument.
 
 LINK-REGEXP is a regexp to make links for MODE-PREFIX-describe-symbol.
-If NIL, MODE-PREFIX-describe-symbol does not make any links in help buffers.
-LINKED-STR-FN is a function which takes substrings matched in LINK-REGEXP
-and returns a string to be linked. MAKE-LINK-FN is a function which takes
-same arguments as LINKED-STR-FN and returns a string which is a link to
-other document. PREFIX-STR and SUFFIX-STR are the prefix and suffix of the
-return value of LINKED-STR-FN respectively.
+If NIL, MODE-PREFIX-describe-symbol does not make any links in help
+buffers.  LINKED-STR-FN is a function which takes substrings matched
+in LINK-REGEXP and returns a string to be linked.  MAKE-LINK-FN is a
+function which takes same arguments as LINKED-STR-FN and returns a
+string which is a link to other document.  PREFIX-STR and SUFFIX-STR
+are the prefix and suffix of the return value of LINKED-STR-FN
+respectively.
 
-For instance, Let LINK-REGEXP be \"`\\\\(.+\\\\)'\", LINKED-STR-FN be
-(lambda (a b) (concat \"[\" b \"]\")), MAKE-LINK-FN be (lambda (a b) b),
-and PREFIX-STR and SUFFIX-STR  are \"`\" and \"'\" respectively.
+For instance, let LINK-REGEXP be \"`\\\\(.+\\\\)'\", LINKED-STR-FN
+be (lambda (a b) (concat \"[\" b \"]\")), MAKE-LINK-FN
+be (lambda (a b) b), and PREFIX-STR and SUFFIX-STR are \"`\" and
+\"'\" respectively.
 
-In this case, a string \"`linked-str'\" becomes
-\"`[linked-str]'\" with a link to \"linked-str\" in help buffer ."
+In this case, a string \"`linked-str'\" becomes \"`[linked-str]'\"
+with a link to \"linked-str\" in help buffer ."
 
   (lexical-let ((mode (intern (concat (symbol-name mode-prefix) "-mode")))
                 (setup (intern (concat (symbol-name mode-prefix) "-setup")))
